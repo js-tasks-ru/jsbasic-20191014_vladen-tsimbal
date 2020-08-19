dc8a4205c0c1c7c6e48ae8831ec76438ab0ebc72
Владен Цимбал <vladentsymbal01@gmail.com>
2020-08-19 22:18:55 +0300

    2-module-1-4-tasks-done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @returns {number}
  */
 function factorial(n) {
-	
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+
+	let sum = n;
+
+	for (var i = n - 1; i > 0; i--) {
+		sum *= i;
+	}
+
+	return sum;
 }
